19c13412bb5cf6cb0f476f5d9e08884e53b1d503
Jennifer Lu <jenniferlu@wustl.edu>
2019-12-03 13:44:03 -0600

    socketio working

--- hangman/server.js
+++ hangman/server.js
@@ -11,10 +11,10 @@ let users = {};
 let ids = {};
 
 io.on('connection', socket => {
-  console.log('New client connected')
+  	console.log('New client connected')
 
 
-  socket.on('login_to_server', function(data) {
+  	socket.on('login_to_server', function(data) {
 		console.log("HEREERE");
 		socket.name = data['user'];
 		ids[data['user']]= socket.id;
@@ -27,10 +27,78 @@ io.on('connection', socket => {
 		for (user in users){
 			userlist.push(user);
 		}
-		// socket.broadcast.to(socket.room).emit('broadcast', {message: data["user"]+" has joined the chat."})
-		// io.sockets.to(socket.room).emit("users_to_client",{ userlist:userlist, room: socket.room});
+		io.sockets.to(socket.room).emit("users_to_client",{ userlist:userlist, game: socket.room});
 		io.sockets.emit("login_to_client",{games: games, users:users}); 
+	});
+
+	socket.on('add_game_to_server', function(data) {
+		// let oldroom = socket.room;
+
+		socket.leave(socket.room);
+		socket.join(data['game']);
+			
+		games[data['game']] = {}
+		games[data['game']]['word'] = data["word"];
+		games[data['game']]['creator'] = data["user"];
+		games[data['game']]['banList'] = [];
 		
+		socket.room = data['game'];
+		users[data['user']] = socket.room;
+		userlist = [];
+		olduserlist = [];
+		for (user in users){
+			if (users[user] == socket.room){
+				userlist.push(user);
+			}
+			else{
+				olduserlist.push(user);
+			}
+		}
+		io.sockets.to(socket.room).emit("users_to_client",{ userlist:userlist, game: socket.room}) 
+		// io.sockets.to(oldroom).emit("oldroom_users_to_client",{olduserlist: olduserlist})
+		io.sockets.emit('update_games_to_client',{game: data["game"]})
+	});
+
+	socket.on('changegame_to_server', function(data) {
+		if (rooms[data['game']]['banList'].includes(data["user"])){
+			socket.emit('banned')
+		}
+		else{
+			console.log(data['user'] + "changing rooms /// joining: " + data['game'] + " /// leaving: "+socket.room)
+			let oldroom = socket.room;
+			socket.leave(socket.room);
+			socket.join(data['game']);
+			socket.room = data['game'];
+
+			// if user is not joining the room they are already in, clear the chat log
+			if(oldroom != socket.room){
+				socket.emit("reset_game")
+			}
+			users[data['user']] = socket.room;
+			userlist = [];
+			olduserlist = [];
+			for (user in users){
+				if (users[user] == socket.room){
+					userlist.push(user);
+				}
+				else if (users[user] == oldroom){
+					olduserlist.push(user);
+				}
+			}
+			io.sockets.to(socket.room).emit("users_to_client",{userlist:userlist, game:socket.room}) 
+			// io.sockets.to(oldroom).emit("oldroom_users_to_client",{olduserlist:olduserlist})
+		}
+	});
+
+	socket.on('disconnect', function(data) {
+		console.log(socket.name)
+		socket.leave(socket.room);
+		delete users[socket.name];
+		userlist = [];
+		for (user in users){
+			userlist.push(user);
+		}
+		io.sockets.to(socket.room).emit("users_to_client",{ userlist:userlist, game: ""});
 	});
 
 })
--- hangman/src/components/game.jsx
+++ hangman/src/components/game.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import io from "socket.io-client";
-const socket = io('localhost:8080');
+const socketio = io('localhost:8080');
 class Game extends Component {
 
     constructor() {
@@ -12,6 +12,7 @@ class Game extends Component {
         this.closeModal = this.closeModal.bind(this) 
         this.openModal = this.openModal.bind(this) 
         this.addGame = this.addGame.bind(this) 
+        this.changegame = this.changegame.bind(this)
 
         this.state = {
           guessesRemaining: 7,
@@ -23,12 +24,11 @@ class Game extends Component {
           word: "America",
           spaces: []
         };
-      }
-    
+    }
     
       componentDidMount = () => {
 
-          socket.on("login_to_client",function(data) {
+        socketio.on("login_to_client",function(data) {
             while(document.getElementById("game-room-list").firstChild){
                 document.getElementById("game-room-list").removeChild(document.getElementById("game-room-list").firstChild);
              }
@@ -36,7 +36,6 @@ class Game extends Component {
              document.getElementById('game-room-list').appendChild(document.createElement("br"));
              let game;
              for (game in data['games']){
-    
                 //create join game buttons
                 let button = document.createElement('input');
                 button.setAttribute('type','button');
@@ -44,7 +43,7 @@ class Game extends Component {
                 button.setAttribute('value', 'Join' );
                 button.setAttribute('class', game);
                 button.style.textAlign = "center";
-                button.setAttribute('onclick', 'changegame(this.id)');
+                button.setAttribute('onClick', '{this.changegame(this.id)}');
     
                 //append buttons to chat room list
                 document.getElementById('game-room-list').appendChild(document.createTextNode(game));
@@ -72,79 +71,167 @@ class Game extends Component {
     
                 document.getElementById('game-room-list').appendChild(document.createElement("br"));
              }
-          })
-      }
+        })
+
+        // UPDATES ROOM LIST AFTER ADDING
+        socketio.on("update_games_to_client",function(data) {
+            //Append an HR thematic break and the escaped HTML of the new message
+            document.getElementById('game-room-list').appendChild(document.createElement("br"));
+
+            // create join game buttons
+            let button = document.createElement('input');
+            button.setAttribute('type','button');
+            button.setAttribute('id', data["game"]);
+            button.setAttribute('value', 'Join' );
+            button.setAttribute('class', data["game"]);
+            button.setAttribute('onClick', '{this.changegame(this.id)}');
+            button.style.textAlign = "center";
+
+            //create watch game buttons
+            let button2 = document.createElement('input');
+            button2.setAttribute('type','button');
+            button2.setAttribute('id', data["game"]);
+            button2.setAttribute('value', 'Watch' );
+            button2.setAttribute('class', data["game"]);
+            button2.setAttribute('onClick', 'watchgame(this.id)');
+            button2.style.textAlign = "center";
+
+            // create delete game buttons
+            let button1 = document.createElement('input');
+            button1.setAttribute('type','button');
+            button1.setAttribute('id', data["game"]);
+            button1.setAttribute('value', 'Delete' );
+            button1.setAttribute('class', data["game"]);
+            button1.setAttribute('onclick', 'deletegame(this.id)');
+            button1.style.textAlign = "center";
+            
+            // append buttons to chat room list
+            document.getElementById('game-room-list').appendChild(document.createTextNode(data["game"]));
+            document.getElementById('game-room-list').appendChild(button);
+            document.getElementById('game-room-list').appendChild(button2);
+            document.getElementById('game-room-list').appendChild(button1);
+        });
+
+        // UPDATES USERS LIST
+        socketio.on("users_to_client",function(data) {
+            while(document.getElementById("users-list").firstChild){
+                document.getElementById("users-list").removeChild(document.getElementById("users-list").firstChild);
+            }
+
+            document.getElementById('users-list').appendChild(document.createElement("br"));
+            let user;
+            for (user in data['userlist']){
+                // ADD PM, KICK, BAN BUTTONS FOR EACH USER
+                let button0 = document.createElement('input');
+                button0.setAttribute('type','button');
+                button0.setAttribute('id', data["userlist"][user]);
+                button0.setAttribute('value', 'Challenge' );
+                button0.setAttribute('class', data["userlist"][user]);
+                button0.setAttribute('onclick', 'challenge(this.id)')
+                button0.style.textAlign = "center";
+                let button = document.createElement('input');
+                button.setAttribute('type','button');
+                button.setAttribute('id', data["userlist"][user]);
+                button.setAttribute('value', 'Kick Out' );
+                button.setAttribute('class', data["userlist"][user]);
+                button.setAttribute('onclick', 'kick_user(this.id)')
+                button.style.textAlign = "center";
+                let button1 = document.createElement('input');
+                button1.setAttribute('type','button');
+                button1.setAttribute('id', data["userlist"][user]);
+                button1.setAttribute('value', 'Ban' );
+                button1.setAttribute('class', data["userlist"][user]);
+                button1.setAttribute('onclick', 'ban_user(this.id)')
+                button1.style.textAlign = "center";
+            
+                document.getElementById('users-list').appendChild(document.createTextNode(data["userlist"][user] + " \t"));
+                document.getElementById('users-list').appendChild(button0);
+                document.getElementById('users-list').appendChild(button);
+                document.getElementById('users-list').appendChild(button1);
+                document.getElementById('users-list').appendChild(document.createElement("br"));
+            }
+
+            // document.getElementById('game-room-name').innerHTML = data['game'];
+        });
+
+        socketio.on("banned", function(){
+            alert("You have been banned from this game.");
+        });
+
+        //RESETS GAME BOARD
+        socketio.on("reset_game", function(){
+            this.setState({guessesRemaining: 7, alphabet : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q','R','S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'], lettersGuessed: [], indices:[], spaces: []});
+            // while(document.getElementById("hangman").firstChild){
+            //     document.getElementById("chatlog").removeChild(document.getElementById("chatlog").firstChild);
+            // } 
+        });
+
+    }
     
 
     render() { 
-
         return ( 
-            
-            <React.Fragment>
-            
-            <div>
-                
-                
-                <div id="options">
-                    
-                    <div style={{display:'none'}} id="logout">
-                        <h3>Logged in as: </h3><div id="user"> </div><br></br>
-                    </div>
-                    <div id="login">
-                    <h2>Log In As?</h2>
-                        Username: <br></br><input style ={{width:90}} type="text" id="username" placeholder="Username" /><br></br>
-                        <button style={{width:90}} onClick = {() => this.login()} id="login_btn">Log In</button>
-                    </div>
-                    <br></br>
-                    <div style={{display:'none'}} id="users">
-                    <h3>Users Online</h3>
-                    <div id="users-list"></div>
-                    </div>
-                </div>
-
-                <div style = {{display:'none'}} id="game-list">
-                        <div id="add-game">
-                        <button id="add-game-btn" onClick = {() => this.openModal()}>New Game</button>
-                        </div>
-                        <div style={{display:'none'}} id="add-game-modal" className="modal">
-                        <div className="modal-content">
-                                <span className="close">&times;</span>
-                                <div className="container">
-                                    <label><b>Game Name: </b></label>
-                                    <input style={{width: 90}} id = "game_name" ref = 'game_name' type="text" placeholder="Enter Game Name" name="game_name" required/>
-                                    <label><b>Word: </b></label>
-                                    <input style={{width: 90}} id = "word" ref = 'word' type="text" placeholder="Enter Word" name="word" required/>
-                                </div>
-                
-                                <div className="container" style={{backgroundColor:'#f1f1f1'}}>
-                                <button id="cancel_btn" onClick = {() => this.closeModal()}>Cancel</button>
-                                <button style={{float:'right'}} id="create_game" onClick = {() => this.addGame()}>Create</button>
-                                </div>
+            <React.Fragment>            
+                <div>
+                    <div>
+                        <div id="options">
+                            <div style={{display:'none'}} id="logout">
+                                <h4>Logged in as: </h4><div id="user"> </div><br></br>
                             </div>
                         </div>
-                        <h3>Game Rooms List</h3>
-                        <div id="game-room-list"></div>
-                </div>
+                    </div>
+                    <div>
+                        <div id="login">
+                            <h2>Log In As?</h2>
+                            Username: <input style ={{width:160}} type="text" id="username" placeholder="Username" />
+                            <button style={{width:70}} onClick = {() => this.login()} id="login_btn">Log In</button>
+                        </div>
+                        <div style = {{display:'none'}} id="game-list">
+                            <div style={{display:'none'}} id="add-game-modal" className="modal">
+                                <div className="modal-content">
+                                    <span className="close">&times;</span>
+                                    <div className="container">
+                                        <label><b>Game Name: </b></label>
+                                        <input style={{width: 90}} id = "game_name" ref = 'game_name' type="text" placeholder="Enter Game Name" name="game_name" required/>
+                                        <label><b>Word: </b></label>
+                                        <input style={{width: 90}} id = "word" ref = 'word' type="text" placeholder="Enter Word" name="word" required/>
+                                    </div>
+                    
+                                    <div className="container" style={{backgroundColor:'#f1f1f1'}}>
+                                    <button id="cancel_btn" onClick = {() => this.closeModal()}>Cancel</button>
+                                    <button style={{float:'right'}} id="create_game" onClick = {() => this.addGame()}>Create</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <h3>Game Rooms List</h3>
+                            <button style={{float:'right'}} id="add-game-btn" onClick = {() => this.openModal()}>New Game</button>
+                            <div id="game-room-list"></div>
+                        </div>
 
-                <div style={{float:'left'}}> <img alt="" src={ require('../stage'+this.state.guessesRemaining+'.png') } /> 
-                <span  >{this.state.spaces.map(blank => <span>{blank}</span> )}</span>
-                </div>
-                
+                        <div style={{display:'none'}} id="users">
+                            <h3>Users Online</h3>
+                            <div id="users-list"></div>
+                        </div>
 
-                <span>You have: {this.state.guessesRemaining} attempts remaining</span>
-                <br></br>
-                {this.state.alphabet.map(l => <button style={{width: 50}} key= {l} onClick = {() => this.LetterGuess(l)}>{l}</button>)}
-                <br></br>
-                <span> Letters Guessed: <br></br>{this.state.lettersGuessed.map(item => <span>{item}</span> )}</span>
-            </div>
-            <script src="/socket.io/socket.io.js"></script>
+                        <div style = {{display:'none'}} id="hangman">
+                            <div style={{float:'left'}}> <img alt="" src={ require('../stage'+this.state.guessesRemaining+'.png') } /> 
+                                <span>{this.state.spaces.map(blank => <span>{blank}</span> )}</span>
+                            </div>
+                            
+                            <span>You have: {this.state.guessesRemaining} attempts remaining</span>
+                            <br></br>
+                            {this.state.alphabet.map(l => <button style={{width: 50}} key= {l} onClick = {() => this.LetterGuess(l)}>{l}</button>)}
+                            <br></br>
+                            <span> Letters Guessed: <br></br>{this.state.lettersGuessed.map(item => <span>{item}</span> )}</span>
+                        </div>
+                    </div>
+                </div>
+                <script src="/socket.io/socket.io.js"></script>
             </React.Fragment>
         );
-        
-
     }
-  
-    
 
     login(){
         if (document.getElementById("username").value !== ""){
@@ -153,44 +240,44 @@ class Game extends Component {
             document.getElementById("user").innerHTML = document.getElementById("username").value;
             document.getElementById("game-list").style.display = "block";
             document.getElementById("users").style.display = "block";
-            document.getElementById("add-game").style.display = "block";
-            document.getElementById("root").style.display = "block";
+            // document.getElementById("add-game").style.display = "block";
+            // document.getElementById("users").style.display = "block";
             
             let user = document.getElementById("username").value
             
-            socket.emit("login_to_server", {user: user});
+            socketio.emit("login_to_server", {user: user});
         }
     }
 
-
-
     openModal(){
         let modal = document.getElementById("add-game-modal");
-        // let span = document.getElementsByClassName("close")[0];
-
         modal.style.display = "block";
-
     }
 
     closeModal(){
         let modal = document.getElementById("add-game-modal");
-        // let span = document.getElementsByClassName("close")[0];
-
         modal.style.display = "none";
-
     }
 
     addGame(){
         let gm = document.getElementById('game_name').value;
-        // let word = document.getElementById('word').value;
-        console.log(gm)
-        // let username = document.getElementById('username').value;
+        let word = document.getElementById('word').value;
+        let username = document.getElementById('username').value;
 
-        // socketio.emit("add_game_to_server", {game: gm, word: word, user: username});
+        socketio.emit("add_game_to_server", {game: gm, word: word, user: username});
         
         this.closeModal();
         // document.getElementById('game-room-name').innerHTML = newgame;
         gm = document.getElementById('game_name').value = "";
+        document.getElementById("users").style.display = "none";
+        document.getElementById("hangman").style.display = "block";
+        this.setBoard();
+    }
+
+    changegame(game) {
+        console.log("going to: " + game);
+        let username = document.getElementById('username').value;
+        socketio.emit("changegame_to_server", {game: game, user:username});
     }
 
     setBoard(){
@@ -214,10 +301,7 @@ class Game extends Component {
                 i.push(index);
                 index = splits.indexOf(letter, index + 1);
             }
-
             this.setState({indices: i});
-            // console.log(this.state.indices);
-
             return true;
         }
         else{
@@ -238,8 +322,6 @@ class Game extends Component {
 
         // If Guess is Wrong
         if (!this.CheckGuess(letter)){
-            
-
             if(this.state.guessesRemaining - 1 >= 0){
                 this.setState({ guessesRemaining: this.state.guessesRemaining - 1});
             }
@@ -258,11 +340,7 @@ class Game extends Component {
             }
             this.setState({spaces: blanks});
         }
-       
-        
     }
-
-    
 }
  
 export default Game;
